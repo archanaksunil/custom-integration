dcd77a608c2b7e01f0553c07b650fce085f80462
Archana K Sunil <aksunil@vmware.com>
2021-06-21 01:35:24 -0600

    larity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClarityModule, ClrIconModule } from '@clr/angular';
+import { IntegrationListComponent } from './integration-list/integration-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntegrationListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClarityModule,
+    ClrIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
