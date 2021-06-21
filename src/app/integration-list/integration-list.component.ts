18088d8b80133d9b3edb55f1286886c0c56d7a3d
Archana K Sunil <aksunil@vmware.com>
2021-06-21 01:12:48 -0600

    Integrate

---
+++ src/app/integration-list/integration-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'integration-list',
+  templateUrl: './integration-list.component.html',
+  styleUrls: ['./integration-list.component.scss']
+})
+export class IntegrationListComponent {
+}
